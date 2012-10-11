f352164a5684e97b2346f73cbedfccfe26a573e8
Muhammad Ashraf <mansoor.ashraf@gmail.com>
2012-10-11 13:49:54 -0500

    finished ex6

--- src/main/scala/com/kohls/ExamplesChapter2.scala
+++ src/main/scala/com/kohls/ExamplesChapter2.scala
@@ -1,5 +1,7 @@
 package com.kohls
 
+import javax.management.remote.rmi._RMIConnection_Stub
+
 /**
  * @author Muhammad Ashraf
  * @since 6/14/12
@@ -8,9 +10,26 @@ package com.kohls
 object ExamplesChapter2 {
 
   type Int2Int = Int => Int
+  type Pred[A] = A => Boolean
 
   class Exercise2 {
-    def absolute(f: Int2Int): Int2Int = x => scala.math.abs(x)
+    def absolute(f: Int2Int): Int2Int = x => scala.math.abs(f(x))
+  }
+
+  class Exercise4 {
+    def divisibleBy[A](k: Int): Pred[Int] = x => x % k == 0
+  }
+
+  class Exercise5 {
+    def even(i: Int) = new Exercise4().divisibleBy(2)(i)
+  }
+
+  class Exercise6 {
+    def isDivisibleBy3And5: Pred[Int] = x => lift[Int]((a, b) => a && b, new Exercise4().divisibleBy(3), new Exercise4().divisibleBy(5))(x)
+
+    def isDivisibleBy3Or5: Pred[Int] = x => lift[Int]((a, b) => a || b, new Exercise4().divisibleBy(3), new Exercise4().divisibleBy(5))(x)
+
+    def lift[A](f: (Boolean, Boolean) => Boolean, g: Pred[A], h: Pred[A]): Pred[A] = x => f(g(x), h(x))
   }
 
 }
--- src/test/scala/com/kohls/ExamplesChapter2Test.scala
+++ src/test/scala/com/kohls/ExamplesChapter2Test.scala
@@ -11,11 +11,31 @@ class ExamplesChapter2Test extends FlatSpec {
 
   trait fixture {
     val ex2 = new ExamplesChapter2.Exercise2
+    val ex4 = new ExamplesChapter2.Exercise4
+    val ex5 = new ExamplesChapter2.Exercise5
+    val ex6 = new ExamplesChapter2.Exercise6
   }
 
   "Absoulate function" should "return only positive values" in new fixture {
     val result: Int = ex2.absolute(x => x * -1)(4)
     assert(result === 4)
   }
+
+  "Divisible By" should "return true when a number is divisible by k" in new fixture {
+    assert(ex4.divisibleBy(2)(4) === true)
+  }
+
+  "Even" should "return true for even numbers" in new fixture {
+    assert(ex5.even(200) === true)
+  }
+
+  "IsDivisible3And5" should "return true for 15" in new fixture {
+    assert(ex6.isDivisibleBy3And5(15))
+  }
+
+  "IsDivisible3Or5" should "return true for 5" in new fixture {
+    assert(ex6.isDivisibleBy3Or5(5)===true)
+  }
+
 
 }
